a9727c0427ac20f48d2e96ce372b24725a9d2562
jose <josemorcelli1@gmail.com>
2022-12-02 20:13:03 -0300

    ajustando detalhes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from "express"
 import cors from "cors"
+import { AddressInfo } from "net"
 
-export const app = express()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, ()=>{
-    console.log("Servidor rodando na porta 3003!")
-
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server){
+        const adress = server.address() as AddressInfo;
+        console.log(`Servidor rodando na porta http://localhost:${adress.address}`);
+    } else {
+        console.error("falha ao iniciar o servidor")
+    }
 })
 
 export default app
